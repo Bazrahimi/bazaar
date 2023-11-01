2e12ab9aad97874f7cb9b8e3e3da77416fc9cc34
Baz Rahimi <bazrahimi@hotmail.com>
2023-11-02 04:18:37 +1100

    add

--- client/dist/assets/navigateToProductDetails-4801012d.js
+++
@@ -1,1 +0,0 @@
-import{p as o}from"./index-7df5b2c6.js";const r=()=>{const t=o();return a=>{t(`/product/${a}`)}};export{r as n};
--- client/src/utils/cloudinaryConfig.js
+++
@@ -1,5 +0,0 @@
-import { Cloudinary } from "@cloudinary/url-gen";
-
-const cld = new Cloudinary({cloud: {cloudName: 'drvh5xeuw'}});
-
-module.exports = cld

58ce374b7b06ac9179fb4374c6d29e16983511eb
Baz Rahimi <bazrahimi@hotmail.com>
2023-11-04 18:25:49 +1100

    resolve

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 // for some reason process.env.is undefined resuls undefined
 const uri = process.env.MONGODB_URI;
 
-// const uri = "mongodb+srv://bazrahimi:jIspud-putqyj-xojti4@cluster0.6nywhlq.mongodb.net/bazaarDB?retryWrites=true&w=majority";
-
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
